223c1a91c777909b46bff33c39bb3749671b6bdf
Edson <edsonmark13@gmail.com>
2023-04-04 09:40:24 +0800

    added function

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
 ((w, d) => {
-    console.log(w.blinkSettings)
+    const setUp = () => {
+        const createDIv = (styles) => {
+            const e = d.createElement('div');
+            for (let style in styles) {
+                div.style[style] = styles[style];
+            }
+            return d.body.appendChild(e), e
+        }
+    }
+
+    let styles = document.head.appendChild(document.createElement("style"));
+    styles.innerHTML = `#widget_container{
+        position: fixed;
+        right: 30px;
+        bottom: 30px;
+        background-color: #3DAC47;
+        padding: 10px 20px;
+        border-radius: 30px;
+        padding: 15px 15px 15px 50px;
+        cursor: pointer;
+        box-shadow: 0 0px 20px 0 rgba(0, 0, 0, .25);
+        border:0;
+        z-index:2147483646;
+        text-align: center;
+        color: white;
+        transition: all 1s;
+    }
+    #widget_container.active{
+        padding: 15px 50px 15px 15px;
+    }
+
+    #widget_img{
+        position: absolute;
+        width: 45px;
+        left: -3%;
+        top: 1px;
+        bottom: 0;
+        transform: rotate(0deg);
+        transition: all 1s;
+    }
+
+    #widget_img.onCall{
+        left: 75%;
+        transform: rotate(360deg);
+    }`
+
 })(window, document)
